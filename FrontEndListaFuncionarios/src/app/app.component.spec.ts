c093f5b60a2f5fd71a0bc273c9661c03bba2bb77
Lucas Freitas <lcffreitas1@gmail.com>
2024-02-21 14:08:43 -0300

    angular change to no-standalone

--- FrontEndListaFuncionarios/src/app/app.component.spec.ts
+++ FrontEndListaFuncionarios/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'FrontEndAngular' title`, () => {
+  it(`should have as title 'FrontEndListaFuncionarios'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FrontEndAngular');
+    expect(app.title).toEqual('FrontEndListaFuncionarios');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FrontEndAngular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FrontEndListaFuncionarios');
   });
 });
